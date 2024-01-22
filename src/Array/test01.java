21f405bc1970574e114db8936429bc9e2c523dc1
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2024-01-22 19:03:42 +0700

    First Array Project

--- src/Array/test01.java
+++ src/Array/test01.java
@@ -1,10 +1,22 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class test01 {
 
+    public static void Xuat(String arr[]) {
+    Scanner input = new Scanner(System.in);
+	for(String car:arr) {
+		System.out.println(car);
+	}
+   }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Input number of cars: ");
+		Scanner input = new Scanner(System.in);
+		String[]cars = new String[] {"BMW", "Toyota", "Chervolet", "Ferrari"};
+		Arrays.sort(cars);
+		Xuat(cars);
 	}
-
 }
